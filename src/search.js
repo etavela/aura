273708c8063d8726a1ce002279cc08e2acdb0780
Eric <etavela@gmail.com>
2021-03-17 09:40:53 -0400

    Adding index search (WIP)

--- src/search.js
+++ src/search.js
@@ -1,21 +1,102 @@
-const zipCodes = require('./data.json')
+const zipCodes = require("./data.json");
 
-function build () {
+const ZIP_INDEX = "zip";
+const CITY_INDEX = "city";
 
-  function byCoordinates (latitude, longitude, numberOfResults) {
-    return zipCodes.filter(zipCode => zipCode.zip.includes(searchValue))
+function build() {
+  const indexes = {
+    zip: {},
+    city: {}
+  };
+
+  initializeIndexes();
+
+  function initializeIndexes() {
+    indexValues(ZIP_INDEX, zipCode => zipCode.zip);
+    indexValues(CITY_INDEX, zipCode => zipCode.primary_city);
+    const acceptableCitySelector = zipCode => 
+      zipCode.acceptable_cities
+      ? zipCode.acceptable_cities.split(",").map(city => city.trim())
+      : [];
+    indexValues(CITY_INDEX, acceptableCitySelector);
   }
 
-  function byZipCode (searchValue) {
-    return zipCodes.filter(zipCode => zipCode.zip.includes(searchValue))
+  function indexValues(indexName, selector) {
+    for (const zipCode of zipCodes) {
+      const values = selector(zipCode);
+      for (const value of values) {
+        indexValue(indexName, value, zipCode, true);
+      }
+    }
+  }
+
+  function indexValue(indexName, value, zipCode, exact) {
+    const index = indexes[indexName];
+    if (!index[value]) {
+      index[value] = [];
+    }
+    index[value].push({
+      zipCode,
+      exact
+    });
+  }
+
+  function byCoordinates(latitude, longitude, numberOfResults) {
+    // return zipCodes.filter(zipCode => zipCode.zip.includes(searchValue))
+  }
+
+  function byIndex(indexName, searchValue, exact) {
+    const index = indexes[indexName];
+    if (!index) {
+      throw new Error("Invalid index name: " + indexName);
+    }
+    if (exact) {
+      return getExactMatches(index, searchValue);
+    } else {
+      return getPartialMatches(index, searchValue);
+    }
+
+    function getExactMatches(index, searchValue) {
+      const matches = index[searchValue];
+      return matches
+        ? matches.filter(match => match.exact).map(match => match.zipCode)
+        : [];
+    }
+
+    function getPartialMatches(index, searchValue) {
+      let matches = index[searchValue];
+      if (!matches) {
+        matches = indexPartialMatches();
+      }
+      return matches.map(match => match.zipCode);
+
+      function indexPartialMatches() {
+        const exactMatchKeys = Object.keys(index).filter(
+          key => index[key].exact
+        );
+        const partialMatches = [];
+        for (const key of exactMatchKeys) {
+          if (key.includes(searchValue)) {
+            const currentPartialMatches = index[key].map(entry => {
+              return { zipCode: entry.zipCode, exact: false };
+            });
+            partialMatches.push(...currentPartialMatches);
+          }
+        }
+        index[searchValue] = partialMatches;
+        return partialMatches;
+      }
+    }
   }
 
   return {
     byCoordinates,
-    byZipCode
-  }
+    byIndex,
+    ZIP_INDEX,
+    CITY_INDEX
+  };
 }
 
 module.exports = {
   build
-}
+};
--- src/search.test.js
+++ src/search.test.js
@@ -1,20 +1,27 @@
 const { handler } = require("./index");
-const builder = require("./search")
+const builder = require("./search");
 
 describe("search", () => {
-  const search = builder.build()
-  describe("byZipCode", () => {
-    test("should return an exact match", () => {
-      const result = search.byZipCode("01002")
-      expect(result.length).toBe(1);
-      const match = result[0]
-      expect(match.zip).toBe("01002")
-    });  
+  const search = builder.build();
+  describe("byIndex()", () => {
+    describe("on zip code", () => {
+      test("should return an exact match", () => {
+        const result = search.byIndex(search.ZIP_INDEX, "01002", true);
+        expect(result.length).toBe(1);
+        const match = result[0];
+        expect(match.zip).toBe("01002");
+      });
 
-    test("should return partial matches", () => {
-      const result = search.byZipCode("0100")
-      expect(result.length).toBe(8);
-      result.forEach(match => expect(match.zip.includes("0100")))
-    });  
-  })
+      test("should return partial match results", () => {
+        const result = search.byIndex(search.ZIP_INDEX, "0100", false);
+        expect(result.length).toBe(8);
+        result.forEach(match => expect(match.zip.includes("0100")));
+      });
+
+      test("should return an empty array if no matches are found", () => {
+        const result = search.byIndex(search.ZIP_INDEX, "9999", false);
+        expect(result.length).toBe(0);
+      });
+    });
+  });
 });
