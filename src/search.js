0edec026b2059ed537043faea2504ec0ede01c0d
Eric <etavela@gmail.com>
2021-03-16 21:44:04 -0400

    Adding coordinate search (WIP)

--- src/search.js
+++ src/search.js
@@ -1,17 +1,17 @@
 const zipCodes = require('./data.json')
 
 function build () {
-  /**
-   * Returns 
-   * 
-   * @param {*} value search for
-   * @returns matching zip codes
-   */
+
+  function byCoordinates(latitude, longitude, numberOfResults) {
+    return zipCodes.filter(zipCode => zipCode.zip.includes(searchValue))
+  }
+
   function byZipCode(searchValue) {
     return zipCodes.filter(zipCode => zipCode.zip.includes(searchValue))
   }
 
   return {
+    byCoordinates,
     byZipCode
   }
 }
