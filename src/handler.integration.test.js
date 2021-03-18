3288396ab71b05b647c53f14d80973f0b93764dd
Eric <etavela@gmail.com>
2021-03-18 03:37:39 -0400

    Added integration tests and missing functionality

--- src/handler.integration.test.js
+++ src/handler.integration.test.js
@@ -6,8 +6,26 @@ describe("integration tests", () => {
       value: "01001"
     });
     const response = await handler(request);
-    expect(response.length).toBe(1);
-    expect(response[0].zip).toBe("01001");
+    checkForZips(response, "01001");
+  });
+
+  test("handler should return multiple city name matches", async () => {
+    const request = buildRequest("/city", {
+      value: "cush"
+    });
+    const response = await handler(request);
+    checkForZips(response, "01002", "02743", "02745", "04109", "04563");
+  });
+
+  test("handler should return filtered multiple city name matches", async () => {
+    const request = buildRequest("/city", {
+      value: "cush",
+      filters: {
+        state: "MA"
+      }
+    });
+    const response = await handler(request);
+    checkForZips(response, "01002", "02743", "02745");
   });
 });
 
@@ -17,4 +35,10 @@ buildRequest = (path, body) => {
     path: path,
     body: JSON.stringify(body)
   };
+};
+
+checkForZips = (response, ...zips) => {
+  const responseZips = new Set(response.map(result => result.zip));
+  expect(responseZips.size).toBe(zips.length);
+  zips.forEach(zip => expect(responseZips.has(zip)).toBe(true));
 };
--- src/service.js
+++ src/service.js
@@ -1,10 +1,16 @@
 build = (search, filter, configuration) => {
+  findByCity = (value, filters) => {
+    const searchResults = search.byIndex(search.CITY_INDEX, value);
+    return filter(searchResults, filters);
+  };
+
   findByZip = (value, filters) => {
     const searchResults = search.byIndex(search.ZIP_INDEX, value);
     return filter(searchResults, filters);
   };
 
   return {
+    findByCity,
     findByZip
   };
 };
